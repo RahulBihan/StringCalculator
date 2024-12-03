1d2ac1271708009fe2d397fc1e41ab4a4388ff0b
Rahul Bihan <bihan.rahul35@gmail.com>
2024-12-04 01:25:19 +0530

    Added Case 3 ; Allow new line

--- string-calculator/src/app/app.component.ts
+++ string-calculator/src/app/app.component.ts
@@ -16,12 +16,15 @@ export class AppComponent {
     this.add("");
     // case 2 : Allow the add method to handle any amount of numbers.
     this.add('1,2,3,4')
+    // case 3 : Allow the add method to handle new lines between numbers (instead of commas). ("1\n2,3" should return 6)
+    this.add('1\n2,3,4\n5')
   }
   add(inputValue:string){
     if(!inputValue){
       return 0;
     }
-    let numArray = inputValue.split(',');
+    const delimiters = [',', '\n'];
+    let numArray = inputValue.split(new RegExp(`[${delimiters.join('')}]`));
     return  numArray.map(n => parseInt(n)).reduce((sum, n) => sum + n, 0);
   }
 }
